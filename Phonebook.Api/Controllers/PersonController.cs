b906e7733cfb225a239e2fe3e7df9297bb6552ef
ronaldorueda <rueda.ronaldo@hotmail.com>
2019-12-27 13:07:12 -0600

    Replacing weather controller with baisc controller

---
+++ Phonebook.Api/Controllers/PersonController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        public List<string> GetPeople()
+        {
+            return new List<string>(){
+                "sauce","omg"
+            }; 
+        }
+    }
+}
